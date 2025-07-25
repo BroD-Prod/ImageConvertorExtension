0175f639dbebd12b18a4c2fd1915fa1be44146b2
BroD-Prod <brodyroth23@gmail.com>
2025-07-25 10:02:33 -0600

    Changing dropHandler and drop_zone to correctly log drop_zone.
    This adds and id to the drop_zone class, so the js can correctly find the class. Now the console logs what the file name was correctly.

--- js/dropzone.js
+++ js/dropzone.js
@@ -10,13 +10,13 @@ function dropHandler(ev){
     if (ev.dataTransfer.items){
         [...ev.dataTransfer.items].forEach((item, i) => {
             if(item.kind === "file"){
-                const file = item.getasFile()
-                console.log('... file[${i}].name = ${file.name}');
+                const file = item.getAsFile()
+                console.log(`... file[${i}].name = ${file.name}`);
             }
         });
     } else {
-        [...ev.dataTransfer.files].forEach((item, i) => {
-            console.log('... file[${i}].name = ${file.name}');
+        [...ev.dataTransfer.files].forEach((file, i) => {
+            console.log(`... file[${i}].name = ${file.name}`);
         });
     }
 }
