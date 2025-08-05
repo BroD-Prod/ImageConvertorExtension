02e131eb0771df96cbe4a1ed921a0014140f5e6e
BroD-Prod <brodyroth23@gmail.com>
2025-08-04 21:22:01 -0400

    Changing html to handle files hsing FIle input type. Dropzone also now handles files and converts them to a hardcoded image type. Does not download or upload files yet.

--- js/dropzone.js
+++ js/dropzone.js
@@ -2,6 +2,11 @@ document.getElementById("drop_zone").addEventListener("drop",  dropHandler);
 
 document.getElementById("drop_zone").addEventListener("dragover", dragOverHandler)
 
+let outputType = 'image/jpg'
+let quality    = 0.8;            
+let width      = undefined;     
+let height     = undefined;
+
 function dropHandler(ev){
     if (ev.dataTransfer.items){
         ev.preventDefault();
@@ -19,11 +24,36 @@ function dropHandler(ev){
         else if (dt.files){
             files = [...dt.files];
         }
-        files.forEach((file, i) => {
-        console.log(`file[${i}].name = ${file.name}`);
-    });
-    console.log("Number of files", files.length)
+        handleFiles(files);
     }
+}
+
+async function handleFiles(files){
+     const convertedList = [];
+     for (const file of files) {
+        if (!file.type.startsWith("image/")) continue;
+
+        const newBlob = await convertImage(file, { type: outputType, quality, width, height });
+        console.log("image converted")
+        const newFile = new File([newBlob], changeExt(file.name, extFromType(outputType)), { type: newBlob.type });
+        convertedList.push(newFile);
+     }
+     
+     console.log("files handled")
+
+     uploadFiles(convertedList);
+
+     console.log("files uploaded")
+}
+
+async function convertImage(inputFile, { type = "image/jpeg", quality=0.8, width, height }) {
+    const bitmap = await createImageBitmap(inputFile);
+
+    const offs = new OffscreenCanvas(width || bitmap.width, height || bitmap.height);
+    const ctx = offs.getContext("2d");
+    ctx.drawImage(bitmap, 0, 0, width || bitmap.width, height || bitmap.height);
+
+    return await offs.convertToBlob({ type, quality });
 }
 
 function dragOverHandler(ev){
